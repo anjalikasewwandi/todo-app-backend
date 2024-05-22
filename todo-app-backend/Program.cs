86285b5aee41ddccedf63e7a40abd5b8bf1a64a7
Anjalika <anjalikastat@gmail.com>
2024-05-22 19:55:07 +0530

    Add the AppDbContext and configures it to use SQLite with the "DefaultConnection"

--- todo-app-backend/Program.cs
+++ todo-app-backend/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add the AppDbContext
+// and configures it to use SQLite with the "DefaultConnection".
+builder.Services.AddDbContext<AppDbContext>(Options =>
+{
+    Options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
