4507a2bef1f7146b0b5e1024f38b8f5324cd54d4
Anjalika <anjalikastat@gmail.com>
2024-05-22 21:07:25 +0530

    Introduce ItemController for managing to-do items

---
+++ todo-app-backend/Controllers/ItemController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using todo_app_backend.Data;
+
+namespace todo_app_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        private readonly AppDbContext appDbContext;
+
+        public ItemController(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+
+    }
+}
